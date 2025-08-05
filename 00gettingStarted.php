9a3ebb5a9e070463f9270a55b18453b4b77bb0ad
Arafat Sarkar <arafatgovt01@gmail.com>
2025-08-05 15:54:23 +0600

    1st commit

---
+++ 00gettingStarted.php
@@ -0,0 +1,12 @@
+<?php
+
+    echo "Hello There How are you? fine? Are you really?<br>";
+    echo "If not, go to a psychologist and take help.<br>";
+    echo "My name is ANON3605 and This is PHP whole basics for laravel<br>";
+    //This is a single line comment that is not shown in the web
+    /*This is a multiline comment,
+      That will not shown
+      in the 
+    */
+?>
+
---
+++ 01variable.php
@@ -0,0 +1,10 @@
+<?php
+    $name="Md. Arafat Sarkar"; //This is a string variable
+    $age=18; //Number variable
+    $male=true; //boolean Variable
+    
+    echo "My name is {$name}<br>";
+    echo "My age is above {$age}<br>";
+    echo "I am a male? {$male}"
+    
+?>
